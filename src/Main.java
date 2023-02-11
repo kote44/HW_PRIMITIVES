2d3198e7090cb74f42f988cbda5229921200247f
KOTE <koss85@gmail.com>
2023-02-11 22:00:17 +0300

    src add

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount1 = 14544;
-        int amount2 = 20;
-        int resolt = amount1 / amount2 * 1;
+        int price = 14544;
+        int unit = 20;
+        int resolt = price  / unit * 1;
 
         System.out.println("Количество бонусных миль: " + resolt);
     }
